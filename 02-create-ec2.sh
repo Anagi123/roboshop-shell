1e017f340331b398c6d37baab7e4ed462137bbc8
Anagi <nageswari.avula@gmail.com>
2023-09-04 15:37:33 +0530

    servers added

--- 02-create-ec2.sh
+++ 02-create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMES=( "mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web" )
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0b5180e3e68542b21
@@ -9,7 +9,7 @@ SECURITY_GROUP_ID=sg-0b5180e3e68542b21
 
 for i in "${NAMES[@]}"
 do
-  if [[ $i == "mongodb" || $i == "mysql" ]]
+  if [[ $i ==  "mongodb" || $i == "mysql" ]]
   then 
       INSTANCE_TYPE="t3.medium"
   else
@@ -17,5 +17,5 @@ do
   fi
     echo "creating $i instance"
     aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids 
-  $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance, Tags=[{Key=name,Value=$i}]"
+    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance, Tags=[{Key=name,Value=$i}]"
 done
