a307cd071e14bcb4bb5a6795392f7303f42f347d
Nageswari <nageswari.avula>
2025-02-06 06:14:39 +0530

    changes

---
+++ sample.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NAMES=$@
+INSTANCE_TYPE=""
+IMAGE_ID=ami-0b4f379183e5706b9
+SECURITY_GROUP_ID=sg-0a10b010ea41f91dd
+DOMAIN_NAME=saidevops.site
+HOSTED_ZONE_ID=Z08187153O1TQ727G81ML
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
+for i in $@
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications 
+
+#     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+#     {
+#             "Changes": [{
+#             "Action": "CREATE",
+#                         "ResourceRecordSet": {
+#                             "Name": "'$i.$DOMAIN_NAME'",
+#                             "Type": "A",
+#                             "TTL": 300,
+#                             "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+#                         }}]
+#     }
+#     '
+# done
+
+# # imporvement
+# # check instance is already created or not
+# check route53 record is already exist, if exist update, otherwise create route53 record
