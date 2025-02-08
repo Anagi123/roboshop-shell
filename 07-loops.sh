39d1307bceee04a85f2204cff1c99be5bbdabc95
Nageswari <nageswari.avula>
2025-02-09 01:06:29 +0530

    changes

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"   
 G="\e[32m"       
@@ -11,15 +15,27 @@ then
     exit 1
 fi   
 
+VALIDATE(){
+    if [$1 -ne 0]
+    then 
+        echo -e "installing $2 ....$R failure $N"
+        exit 1
+    else
+        echo -e "installing $2 ....$G success $N"
+    fi    
+}
+
 for i in $@
 do
-    yum installed $i 
+    yum list installed $i &>>LOGFILE
     if [ $? -ne 0 ]
     then
-        echo "$i is not installed,lets install it"
-        yum install $i -y
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>LOGFILE
+        VALIDATE $? "$i"
     else
         echo -e "$Y $i installed $N"
+    fi
+done    
 
-done            
 
