1da46529413bcbb7a49d40f6bd15f599b8b2bebe
Nageswari <nageswari.avula>
2025-02-08 21:56:09 +0530

    changes

--- 02-install-mysql.sh
+++ 02-install-mysql.sh
@@ -10,3 +10,21 @@ then
 fi
 
 yum install mysqllll -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql is error"
+    exit 1
+else
+    echo "installation of mysql is success"   
+fi     
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of postfix is error"
+    exit 1
+else
+    echo "installation of postfix in success"
+fi        
