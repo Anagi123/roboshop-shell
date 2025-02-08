fa65bea2c4dc1a0d33c09ae9457d65372372c623
Nageswari <nageswari.avula>
2025-02-09 00:42:34 +0530

    changes

--- 07-loops.sh
+++ 07-loops.sh
@@ -4,14 +4,21 @@ USERID=$(id -u)
 R="\e[31m"   
 G="\e[32m"       
 N="\e[0m"
-
+Y="\e[33m"
 if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR:: please run with root access $N"
-    exit
+    exit 1
 fi   
 
 for i in $@
 do
-    yum install $i -y
-done    
+    yum installed $i 
+    if[ $? -ne 0 ]
+    then
+        echo "$i is not installed,lets install it"
+        yum install $i -Y
+    else
+        echo "$Y $i installed $N"
+done            
+
